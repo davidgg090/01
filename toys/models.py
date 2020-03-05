234fbd5523dd02338324dcd4557d066a988afd68
davidgg090 <davidgg090@gmail.com>
2020-03-05 00:32:19 -0300

    created a toy model

--- toys/models.py
+++ toys/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Toy(models.Model):
+    """
+        Represent and Persist toys.
+    """
+
+    created = models.DateTimeField(auto_now=True)
+    name = models.CharField( max_length=150, blank=False, default='')
+    description = models.CharField( max_length=250, blank=True, default='')
+    toy_catergory = models.CharField(max_length=200, blanck=False, default='')
+    release_date = models.DateTimeField()
+    was_included_in_home = models.BooleanField(default=False)
+
+
+    class Meta:
+        """Meta definition for Toy."""
+
+        verbose_name = 'Toy'
+        verbose_name_plural = 'Toys'
+
+    def __str__(self):
+        """Unicode representation of Toy."""
+        return self.name
