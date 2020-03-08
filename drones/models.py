bf3e51d0a732a969b7f71f86d9a87e628d729b05
davidgg090 <davidgg090@gmail.com>
2020-03-08 15:30:13 -0300

    Fixes

--- drones/models.py
+++ drones/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 
+
 class DroneCategory(models.Model):
     name = models.CharField(max_length=250)
     
-
     class Meta:
-        verbose_name = ("DroneCategory")
-        verbose_name_plural = ("DroneCategorys")
-
+        ordering = ('name',)
+    
     def __str__(self):
         return self.name
 
 
-
 class Drone(models.Model):
     name = models.CharField(max_length=250)
-    drone_category = models.ForeignKey(DroneCategory, related_name="drones", on_delete=models.CASCADE)
+    drone_category = models.ForeignKey(
+        DroneCategory,
+        related_name='drones',
+        on_delete=models.CASCADE)
     manufacturing_date = models.DateTimeField()
     has_it_competed = models.BooleanField(default=False)
-    inserted_timestamp = models.DateTimeField(auto_now=True)
+    inserted_timestamp = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        verbose_name = ("Drone")
-        verbose_name_plural = ("Drones")
         ordering = ('name',)
 
     def __str__(self):
@@ -35,26 +34,34 @@ class Pilot(models.Model):
     GENDER_CHOICES = (
         (MALE, 'Male'),
         (FEMALE, 'Female'),
-    ) 
+    )
     name = models.CharField(max_length=150, blank=False, default='')
     gender = models.CharField(
         max_length=2,
         choices=GENDER_CHOICES,
         default=MALE,
-    ) 
+    )
     races_count = models.IntegerField()
     inserted_timestamp = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ('name',)
+    
     def __str__(self):
         return self.name
 
 
 class Competition(models.Model):
-    pilot = models.ForeignKey( Pilot,related_name='competitions',on_delete=models.CASCADE)
-    drone = models.ForeignKey(Drone,on_delete=models.CASCADE)
+    pilot = models.ForeignKey(
+        Pilot,
+        related_name='competitions',
+        on_delete=models.CASCADE)
+    drone = models.ForeignKey(
+        Drone,
+        on_delete=models.CASCADE)
     distance_in_feet = models.IntegerField()
     distance_achievement_date = models.DateTimeField()
+    
     class Meta:
         # Order by distance in descending order
         ordering = ('-distance_in_feet',)
--- drones/views.py
+++ drones/views.py
@@ -4,16 +4,18 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from drones.models import DroneCategory, Drone, Pilot, Competition
 from drones.serializers import DroneCategorySerializer, DroneSerializer, PilotSerializer, PilotCompetitionSerializer
-
 class DroneCategoryList(generics.ListCreateAPIView):
     queryset = DroneCategory.objects.all()
-    serializer_class = DroneSerializer
+    serializer_class = DroneCategorySerializer
     name = 'dronecategory-list'
+
 
 class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = DroneCategory.objects.all()
     serializer_class = DroneCategorySerializer
     name = 'dronecategory-detail'
+
+
 
 class DroneList(generics.ListCreateAPIView):
     queryset = Drone.objects.all()
@@ -25,12 +27,12 @@ class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = DroneSerializer
     name = 'drone-detail'
 
-class PilotoList(generics.ListCreateAPIView):
+class PilotList(generics.ListCreateAPIView):
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
     name = 'pilot-list'
 
-class PilotoDetail(generics.RetrieveUpdateDestroyAPIView):
+class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
     name = 'pilot-detail'
@@ -48,12 +50,12 @@ class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 
-class APiRoot(generics.GenericAPIView):
+class ApiRoot(generics.GenericAPIView):
     name = 'api-root'
     def get(self, request, *args, **kwargs):
         return Response({
             'drone-categories':reverse(DroneCategoryList.name, request=request),
             'drones': reverse(DroneList.name, request=request),
-            'pilots': reverse(PilotoList.name, request=request),
+            'pilots': reverse(PilotList.name, request=request),
             'competitions': reverse(CompetitionList.name, request=request)
         })
