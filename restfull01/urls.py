1e43a85cc506549cbb23a4fdd5e8a507927d0a0d
davidgg090 <davidgg090@gmail.com>
2020-03-08 19:04:02 -0300

    testing

--- restfull01/urls.py
+++ restfull01/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('drones.urls', namespace='v1')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('drones.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
     # url(r'^v2/', include('drones.v2.urls', namespace='v2')),
     # url(r'^v2/api-auth/', include('rest_framework.urls', namespace='rest_framework_v2'))
 ]
