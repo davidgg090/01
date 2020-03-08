0abdc068429026678a7ed00fd3e396ac19fe76f0
davidgg090 <davidgg090@gmail.com>
2020-03-08 16:31:59 -0300

    custom filter

--- drones/views.py
+++ drones/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from django_filters import AllValuesFilter, DateTimeFilter, NumberFilter
 from drones.models import DroneCategory, Drone, Pilot, Competition
 from drones.serializers import DroneCategorySerializer, DroneSerializer, PilotSerializer, PilotCompetitionSerializer
+from drones.filter import CompetitionFilter
+
+
 class DroneCategoryList(generics.ListCreateAPIView):
     queryset = DroneCategory.objects.all()
     serializer_class = DroneCategorySerializer
     name = 'dronecategory-list'
+    filter_fields = ('name',)
+    search_fields = ('^name',)
+    ordering_fields = ('name',)
 
 
 class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -16,21 +23,39 @@ class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     name = 'dronecategory-detail'
 
 
-
 class DroneList(generics.ListCreateAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
     name = 'drone-list'
+    filter_fields = ('name', 'drone_category',
+                     'manufacturing_date', 'has_it_competed',)
+    search_fields = ('^name',)
+    ordering_fields = ('name', 'manufacturing_date',)
+
 
 class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
     name = 'drone-detail'
 
+
 class PilotList(generics.ListCreateAPIView):
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
     name = 'pilot-list'
+    filter_fields = (
+        'name',
+        'gender',
+        'races_count',
+    )
+    search_fields = (
+        '^name',
+    )
+    ordering_fields = (
+        'name',
+        'races_count'
+    )
+
 
 class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pilot.objects.all()
@@ -43,18 +68,20 @@ class CompetitionList(generics.ListCreateAPIView):
     serializer_class = PilotCompetitionSerializer
     name = 'competition-list'
 
+
 class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Competition.objects.all()
     serializer_class = PilotCompetitionSerializer
     name = 'competition-detail'
-
-
+    filter_class = CompetitionFilter
+    ordering_fields = ('distance_in_feet','distance_achievement_date',)
 
 class ApiRoot(generics.GenericAPIView):
     name = 'api-root'
+
     def get(self, request, *args, **kwargs):
         return Response({
-            'drone-categories':reverse(DroneCategoryList.name, request=request),
+            'drone-categories': reverse(DroneCategoryList.name, request=request),
             'drones': reverse(DroneList.name, request=request),
             'pilots': reverse(PilotList.name, request=request),
             'competitions': reverse(CompetitionList.name, request=request)
