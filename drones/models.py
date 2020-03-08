745ca4224c3a5030df13cffbe01c8307b29b4ea1
davidgg090 <davidgg090@gmail.com>
2020-03-08 02:29:12 -0300

    Update models.py

--- drones/models.py
+++ drones/models.py
@@ -54,7 +54,7 @@ class Competition(models.Model):
     pilot = models.ForeignKey( Pilot,related_name='competitions',on_delete=models.CASCADE)
     drone = models.ForeignKey(Drone,on_delete=models.CASCADE)
     distance_in_feet = models.IntegerField()
-    distance_achievement_date = models.Date()
+    distance_achievement_date = models.DateTimeField()
     class Meta:
         # Order by distance in descending order
         ordering = ('-distance_in_feet',)
